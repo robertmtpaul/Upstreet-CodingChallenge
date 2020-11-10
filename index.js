e8689ccfad37500363b6e82bd5bcacd70c9e6e2d
robertmtpaul <rob@glo.id.au>
2020-11-10 22:49:57 +1100

    Iniitalised express server for testing API request. Server starting successfully with nodemon checks

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const axios = require('axios');
 const express = require('express');
 const app = express();
+const PORT = 8080;
 
-const port = 1337;
+// define first route for the home page
+app.get("/", (request, response) => {
+    response.send("Testing the first route");
+})
+
+// start express server
+app.listen(PORT, () => {
+    console.log(`Now serving on at http://localhost:${PORT}...`);
+});
