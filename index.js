c61643058d9614a5828ad9e43ae7af0309c65a15
robertmtpaul <rob@glo.id.au>
2020-11-12 18:32:30 +1100

    Added test for testing response data

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const axios = require('axios');
+const { JsxEmit, idText } = require('typescript');
 const BASE_URL = "https://australia-southeast1-reporting-290bc.cloudfunctions.net/driverlicence"
 const APIKEY = "03aa7ba718da920e0ea362c876505c6df32197940669c5b150711b03650a78cf";
-
+// Pass in input for the function for validation by API
 async function doKyc(BirthDate, GivenName, MiddleName, FamilyName, LicenceNumber, StateOfIssue, ExpiryDate) {
     const props = {
         url: BASE_URL,
@@ -20,6 +21,7 @@ async function doKyc(BirthDate, GivenName, MiddleName, FamilyName, LicenceNumber
         "stateOfIssue": StateOfIssue,
         "expiryDate": ExpiryDate,
     });
+
     if (response.verificationResultCode === "N") {
         console.log('False')
         return false
@@ -35,7 +37,27 @@ async function doKyc(BirthDate, GivenName, MiddleName, FamilyName, LicenceNumber
             message: "Server Error"
         }
     } else {
+        console.log('True')
         return true
     }
 
 }
+
+jest.mock('axios');
+
+it('returns DriverLicenseResponse', async() => {
+            axios.post.mockResolvedValue({
+                    data: [{
+                            verifyDocumentResult: {
+                                type: "DriverLicenceResponse"
+                            },
+                            {
+                                verificationRequestNumber: 00000,
+                                verificationResultCode: "N"
+                            }
+                        ]
+                    });
+
+                const type = await getRequestType(); expect(type).toEqual('DriverLicenseResponse');
+
+            });
