1c7c1196cd15a72c7ead04d14826abe40c2faa16
robertmtpaul <rob@glo.id.au>
2020-11-13 09:25:30 +1100

    Jest set up: added test to scripts in package.json, and test.conf.js for configuration, created index.test.js

--- index.js
+++ index.js
@@ -42,22 +42,3 @@ async function doKyc(BirthDate, GivenName, MiddleName, FamilyName, LicenceNumber
     }
 
 }
-
-jest.mock('axios');
-
-it('returns DriverLicenseResponse', async() => {
-            axios.post.mockResolvedValue({
-                    data: [{
-                            verifyDocumentResult: {
-                                type: "DriverLicenceResponse"
-                            },
-                            {
-                                verificationRequestNumber: 00000,
-                                verificationResultCode: "N"
-                            }
-                        ]
-                    });
-
-                const type = await getRequestType(); expect(type).toEqual('DriverLicenseResponse');
-
-            });
---
+++ index.test.js
@@ -0,0 +1,18 @@
+jest.mock('axios');
+
+it('returns DriverLicenseResponse', async() => {
+            axios.post.mockResolvedValue({
+                    data: [{
+                            verifyDocumentResult: {
+                                type: "DriverLicenceResponse"
+                            },
+                            {
+                                verificationRequestNumber: 00000,
+                                verificationResultCode: "N"
+                            }
+                        ]
+                    });
+
+                const type = await getRequestType(); expect(type).toEqual('DriverLicenseResponse');
+
+            });
