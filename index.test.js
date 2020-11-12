44858dc10e310d9e565511c0866163c72eb9664f
robertmtpaul <rob@glo.id.au>
2020-11-13 09:37:55 +1100

    Installed yarn and initiliased jest as dev dependency

--- index.test.js
+++ index.test.js
@@ -16,3 +16,21 @@ it('returns DriverLicenseResponse', async() => {
                 const type = await getRequestType(); expect(type).toEqual('DriverLicenseResponse');
 
             });
+
+
+        it('returns DriverLicenseResponse', async() => {
+                axios.post.mockResolvedValue({
+                        data: [{
+                                verifyDocumentResult: {
+                                    type: "DriverLicenceResponse"
+                                },
+                                {
+                                    verificationRequestNumber: 00000,
+                                    verificationResultCode: "N"
+                                }
+                            ]
+                        });
+
+                    const type = await getRequestType(); expect(type).toEqual('DriverLicenseResponse');
+
+                });
---
+++ test/jest.conf.js
@@ -0,0 +1,5 @@
+const = require('path');
+
+module.exports = {
+    rootDir: path.resolve()
+}
