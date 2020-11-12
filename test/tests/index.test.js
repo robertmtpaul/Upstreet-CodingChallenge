f7628d965e15ccef843e8ab4cbb1af8c5a4095f5
robertmtpaul <rob@glo.id.au>
2020-11-13 09:57:42 +1100

    Moved jest config file back to root directory to ensure jest able to automatically find it

--- test/tests/index.test.js
+++ test/tests/index.test.js
@@ -1,36 +1,38 @@
 jest.mock('axios');
 
-it('returns DriverLicenseResponse', async() => {
-            axios.post.mockResolvedValue({
-                    data: [{
-                            verifyDocumentResult: {
-                                type: "DriverLicenceResponse"
-                            },
-                            {
-                                verificationRequestNumber: 00000,
-                                verificationResultCode: "N"
-                            }
-                        ]
-                    });
-
-                const type = await getRequestType(); expect(type).toEqual('DriverLicenseResponse');
-
-            });
-
-
-        it('returns DriverLicenseResponse', async() => {
-                axios.post.mockResolvedValue({
-                        data: [{
-                                verifyDocumentResult: {
-                                    type: "DriverLicenceResponse"
-                                },
-                                {
-                                    verificationRequestNumber: 00000,
-                                    verificationResultCode: "N"
-                                }
-                            ]
-                        });
-
-                    const type = await getRequestType(); expect(type).toEqual('DriverLicenseResponse');
-
+it('returns true', async() => {
+        axios.post.mockResolvedValue({
+                data: [{
+                        verifyDocumentResult: {
+                            type: "DriverLicenceResponse"
+                        },
+                        {
+                            verificationRequestNumber: 00000,
+                            verificationResultCode: "N"
+                        }
+                    ]
                 });
+
+            const type = await getRequestType(); expect(type).toEqual('DriverLicenseResponse');
+
+        });
+}
+
+
+it('returns DriverLicenseResponse', async() => {
+        axios.post.mockResolvedValue({
+                data: [{
+                        verifyDocumentResult: {
+                            type: "DriverLicenceResponse"
+                        },
+                        {
+                            verificationRequestNumber: 00000,
+                            verificationResultCode: "N"
+                        }
+                    ]
+                });
+
+            const type = await getRequestType(); expect(type).toEqual('DriverLicenseResponse');
+
+        });
+}
