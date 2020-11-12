94467fe97e6804f1abb82dd1c3457f04c4fcd19d
robertmtpaul <rob@glo.id.au>
2020-11-12 13:06:49 +1100

    Added conditional statement to check axios request based on data passed into doKyc function

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 const axios = require('axios');
-const express = require('express');
-const app = express();
-const PORT = 8080;
+const BASE_URL = "https://australia-southeast1-reporting-290bc.cloudfunctions.net/driverlicence"
+const APIKEY = "03aa7ba718da920e0ea362c876505c6df32197940669c5b150711b03650a78cf";
 
-// define first route for the home page
-app.get("/", (request, response) => {
-    response.send("Testing the first route");
-})
+async function doKyc(BirthDate, GivenName, MiddleName, FamilyName, LicenceNumber, StateOfIssue, ExpiryDate) {
+    const props = {
+        url: BASE_URL,
+        headers: {
+            'Content-Type': 'application/json',
+            'token': APIKEY
+        }
+    }
 
-// start express server
-app.listen(PORT, () => {
-    console.log(`Now serving on at http://localhost:${PORT}...`);
-});
+    const response = await axios.post(`${BASE_URL}`, props, {
+        "birthDate": BirthDate,
+        "givenName": GivenName,
+        "middleName": MiddleName,
+        "familyName": FamilyName,
+        "licenceNumber": LicenceNumber,
+        "stateOfIssue": StateOfIssue,
+        "expiryDate": ExpiryDate,
+    });
+    if (response.verificationResultCode === "N") {
+        console.log('False')
+        return false
+    } else {
+        return true
+    }
+
+}
