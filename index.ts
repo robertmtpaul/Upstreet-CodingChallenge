747c9a9c10c8eb5c78c6ef5adaaae329a42ea74f
robertmtpaul <rob@glo.id.au>
2020-11-13 15:45:32 +1100

    Added mockedAxios tests to check input of doKycCheck. Added data validation for the values input into the doKycCheck including birth date in valid format, and names within character limit.

--- index.ts
+++ index.ts
@@ -3,6 +3,42 @@ const BASE_URL = "https://australia-southeast1-reporting-290bc.cloudfunctions.ne
 const APIKEY = "03aa7ba718da920e0ea362c876505c6df32197940669c5b150711b03650a78cf";
 // Pass in input for the function for validation by API
 export async function doKycCheck(BirthDate, GivenName, MiddleName, FamilyName, LicenceNumber, StateOfIssue, ExpiryDate) {
+
+    // Nested functions for carrying out data validation.
+    function isValidDob() {
+        // Using regular expression check if the BirthDate passed in is in correct format, and if not, return false
+        if(!/^\d{4}-\d{2}-\d{2}$/.test(BirthDate)){
+            console.log('Birth date in invalid format') 
+            return false;
+        }            
+    }
+
+
+    // Set up a function to reject input that is over 100 characters
+    function checkLengths(){
+        if( GivenName.length >= 100){
+            return false
+        }
+        else if( MiddleName.length >= 100){
+            return false
+        } else if( FamilyName.length >=100){
+            return false
+        }
+    }
+    // Fail if license number is missing.
+    function checkLicenseNumber(){
+        if (input === null ){
+            return false
+        }
+    }
+
+    isValidDob();
+    checkLengths();
+    // // checkLength();
+    // // checkLength();
+    // checkLicenseNumber();
+
+
     const props = {
         url: BASE_URL,
         headers: {
@@ -20,6 +56,8 @@ export async function doKycCheck(BirthDate, GivenName, MiddleName, FamilyName, L
         "stateOfIssue": StateOfIssue,
         "expiryDate": ExpiryDate,
     });
+
+    console.log(response)
 
     if (response.verificationResultCode === "N") {
         console.log('False')
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,16 +1,36 @@
 import axios from 'axios';
 jest.mock('axios');
+// Assign typed mock to new variable
 const mockedAxios = axios as jest.Mocked<typeof axios>;
 
+// Run simple test to make sure that jest test suite is working.
 import { doKycCheck } from '../index';
 it('returns true', async() => {
     expect(true).toBe(true);
 });
 
-
-it('should mock something', async () => {
-    axios.post.mockResolvedValue({ data: 'moo cow' });
+// Check if a date of birth is sent in axios post request 
+it('submits a date of birth ', async () => {
+    mockedAxios.post.mockResolvedValue({ 
+        data: {
+            "birthDate" : "1985-02-08",
+        }
+      });
     const response = await doKycCheck();
-    expect(response).toEqual({ data: 'moo cow'});
-  });
+    expect(response).toBe("1985-02-08")
+    });
 
+// Check if a given name is sent in axios post request
+it('submits a given name', async()=> {
+    mockedAxios.post.mockResolvedValue({
+        data: {
+            "givenName" : "James"
+        }
+    });
+    const response = await doKycCheck();
+    expect(response).toBe({
+        data: {
+            "givenName" : "James"
+        }
+    });
+});
